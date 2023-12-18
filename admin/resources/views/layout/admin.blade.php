654b13e5e6173ea9a051c21d1910f51cf44cdd83
baradadi-barad <baradaditya1525@gmail.com>
2023-12-18 23:12:00 +0530

    impliment

---
+++ admin/resources/views/layout/admin.blade.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Star Admin2 </title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{asset('vendors/feather/feather.css')}}">
+    <link rel="stylesheet"
+      href="{{asset('vendors/mdi/css/materialdesignicons.min.cs')}}s">
+    <link rel="stylesheet" href="{{asset('vendors/ti-icons/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/typicons/typicons.css')}}">
+    <link rel="stylesheet"
+      href="{{asset('vendors/simple-line-icons/css/simple-line-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/css/vendor.bundle.base.css')}}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet"
+      href="{{asset('vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}">
+    <link rel="stylesheet" type="text/css"
+      href="{{asset('js/select.dataTables.min.css')}}">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{asset('css/vertical-layout-light/style.css')}}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{asset('images/favicon.png')}}" />
+  </head>
+
+  <body class="with-welcome-text">
+    <div class="container-scroller">
+      <!-- partial:partials/_navbar.html -->
+      <nav
+        class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex align-items-top flex-row">
+        <div
+          class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
+          <div class="me-3">
+            <button class="navbar-toggler navbar-toggler align-self-center"
+              type="button" data-bs-toggle="minimize">
+              <span class="icon-menu"></span>
+            </button>
+          </div>
+          <div>
+            <a class="navbar-brand brand-logo" href="../index.html">
+              <img src="../images/logo.svg" alt="logo" />
+            </a>
+            <a class="navbar-brand brand-logo-mini" href="../index.html">
+              <img src="../images/logo-mini.svg" alt="logo" />
+            </a>
+          </div>
+        </div>
+        <div class="navbar-menu-wrapper d-flex align-items-top">
+          <ul class="navbar-nav">
+            <li class="nav-item font-weight-semibold d-none d-lg-block ms-0">
+              <h1 class="welcome-text">Good Morning,<span class="text-black fw-bold">John Doe</span></h1>
+              <h3 class="welcome-sub-text">Your performance summary this week
+            </h3>
+            </li>
+          </ul>
+          <ul class="navbar-nav ms-auto">
+            
+            <li class="nav-item d-none d-lg-block">
+              <div id="datepicker-popup"
+                class="input-group date datepicker navbar-date-picker">
+                <span class="input-group-addon input-group-prepend border-right">
+                  <span class="icon-calendar input-group-text calendar-icon"></span>
+                </span>
+                <input type="text" class="form-control">
+              </div>
+            </li>
+            <li class="nav-item">
+              <form class="search-form" action="#">
+                <i class="icon-search"></i>
+                <input type="search" class="form-control"
+                  placeholder="Search Here" title="Search here">
+              </form>
+            </li>
+            <li class="nav-item dropdown d-none d-lg-block user-dropdown">
+              <a class="nav-link" id="UserDropdown" href="#"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                <img class="img-xs rounded-circle"
+                  src="../images/faces/face8.jpg" alt="Profile image">
+              </a>
+              <div class="dropdown-menu dropdown-menu-right navbar-dropdown"
+                aria-labelledby="UserDropdown">
+                <div class="dropdown-header text-center">
+                  <img class="img-md rounded-circle"
+                    src="../images/faces/face8.jpg" alt="Profile image">
+                  <p class="mb-1 mt-3 font-weight-semibold">Allen Moreno</p>
+                  <p class="fw-light text-muted mb-0">allenmoreno@gmail.com</p>
+                </div>
+                <a class="dropdown-item"><i
+                    class="dropdown-item-icon mdi mdi-account-outline text-primary me-2"></i>
+                  My
+                  Profile <span class="badge badge-pill badge-danger">1</span></a>
+                <a class="dropdown-item"><i
+                    class="dropdown-item-icon mdi mdi-message-text-outline text-primary me-2"></i>
+                  Messages</a>
+                <a class="dropdown-item"><i
+                    class="dropdown-item-icon mdi mdi-calendar-check-outline text-primary me-2"></i>
+                  Activity</a>
+                <a class="dropdown-item"><i
+                    class="dropdown-item-icon mdi mdi-help-circle-outline text-primary me-2"></i>
+                  FAQ</a>
+                <a class="dropdown-item"><i
+                    class="dropdown-item-icon mdi mdi-power text-primary me-2"></i>Sign
+                  Out</a>
+              </div>
+            </li>
+          </ul>
+          <button
+            class="navbar-toggler navbar-toggler-right d-lg-none align-self-center"
+            type="button"
+            data-bs-toggle="offcanvas">
+            <span class="mdi mdi-menu"></span>
+          </button>
+        </div>
+      </nav>
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial -->
+        <!-- partial:partials/_sidebar.html -->
+        <nav class="sidebar sidebar-offcanvas" id="sidebar">
+          <ul class="nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="../index.html">
+                <i class="mdi mdi-grid-large menu-icon"></i>
+                <span class="menu-title">Dashboard</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <!-- partial -->
+        <div class="main-panel">
+          <div class="content-wrapper">
+          </div>
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+          <footer class="footer">
+            <div
+              class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span
+                class="text-muted text-center text-sm-left d-block d-sm-inline-block">Premium
+                <a
+                  href="https://www.bootstrapdash.com/" target="_blank">Bootstrap
+                  admin template</a> from BootstrapDash.</span>
+              <span
+                class="float-none float-sm-end d-block mt-1 mt-sm-0 text-center">Copyright
+                © 2021. All rights reserved.</span>
+            </div>
+          </footer>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+
+    <!-- plugins:js -->
+    <script src="{{asset('vendors/js/vendor.bundle.base.js')}}"></script>
+    <script
+      src="{{asset('vendors/bootstrap-datepicker/bootstrap-datepicker.min.js')}}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="{{asset('vendors/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('vendors/progressbar.js/progressbar.min.js')}}"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{asset('js/off-canvas.js')}}"></script>
+    <script src="{{asset('js/hoverable-collapse.js')}}"></script>
+    <script src="{{asset('js/template.js')}}"></script>
+    <script src="{{asset('js/settings.js')}}"></script>
+    <script src="{{asset('js/todolist.js')}}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="{{asset('js/jquery.cookie.js')}}" type="text/javascript"></script>
+    <script src="{{asset('js/dashboard.js')}}"></script>
+    <script src="{{asset('js/proBanner.js')}}"></script>
+    <!-- <script src="../../js/Chart.roundedBarCharts.js"></script> -->
+    <!-- End custom js for this page-->
+  </body>
+
+</html>
--- admin/routes/web.php
+++ admin/routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
