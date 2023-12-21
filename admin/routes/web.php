1ac173c349b417d54a847097b80ebdf24c45609f
baradadi-barad <baradaditya1525@gmail.com>
2023-12-21 21:08:41 +0530

    aditya-12-12-23

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
+Route::middleware('login')->group(function () {
+    Route::get('home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
+});
